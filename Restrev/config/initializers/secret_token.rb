87ac72c365fa455e90317be65d21035e795774aa
ridingrails <student@appacademy.io>
2014-03-09 11:34:40 -0700

    adding first

---
+++ Restrev/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Restrev::Application.config.secret_token = '42b15a1f7e2431d12bec042d4661d095ae9b73575f943c1f61739c9579eca83603d0715d46ac7c251ad88a4ffb12d61808e942afcce01fb5517ddf404adfe072'
